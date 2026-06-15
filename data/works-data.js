5ae7e604c6546a4df3699d67056df97e20024df8
490912195 <490912195@qq.com>
2026-06-15 13:23:20 +0800

    Update works data from online admin

--- data/works-data.js
+++ data/works-data.js
@@ -6,5 +6,13 @@ const SERVER_WORKS = [
     "desc": "",
     "sub": "",
     "img": "images/902A6464.jpg"
+  },
+  {
+    "id": 2,
+    "cat": "tang",
+    "title": "902A6415",
+    "desc": "",
+    "sub": "",
+    "img": "images/902A6415.jpg"
   }
 ];
